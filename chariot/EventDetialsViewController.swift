f354fd2abda531f91fdaec9d756f5790fb339dbe
reesecrowell <crowellreese@gmail.com>
2022-11-28 13:14:42 -0500

    added geofence around event for event details view

---
+++ chariot/EventDetialsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  EventDetialsViewController.swift
+//  chariot
+//
+//  Created by Reese Crowell on 11/28/22.
+//
+
+import UIKit
+import MapKit
+
+class EventDetialsViewController: UIViewController {
+
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var eventBoundaryMap: MKMapView!
+    
+    
+    var lat: CLLocationDegrees = 40.42383268068071
+    var long: CLLocationDegrees = -86.91261817785627
+    var radius: Double = 500
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let eventLocation = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let viewRegion = MKCoordinateRegion(center: eventLocation, latitudinalMeters: radius * 1.25, longitudinalMeters: radius * 1.25)
+        let region = CLCircularRegion(center: eventLocation, radius: 5000, identifier: "geofence")
+        eventBoundaryMap.addOverlay(MKCircle(center: eventLocation, radius: 200))
+        eventBoundaryMap.setRegion(viewRegion, animated: false)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
